62de032e5566d7aa1a5c4df28c9c1ccae047415c
Johannes Giercksky Nilssen <johanngn@stud.ntnu.no>
2021-03-26 17:55:59 +0100

    lp-norm regularization implemented

--- overparametirized.py
+++ overparametirized.py
@@ -5,33 +5,27 @@ from matplotlib.cm import viridis
 from scipy.integrate import solve_ivp
 from scipy.integrate._ivp.ivp import OdeResult
 
-# Set seed to repeat experiment
-np.random.seed(69)
+# Set random seed to repeat experiment
+np.random.seed(99)
 
 # Shortcut for Euclidian norm
 l2 = np.linalg.norm
 
-def l2_regularizer(x):
-    n = len(x)
-    # Avoid division by zero
-    if l2(x) == 0:
-        return np.zeros((n,n))
-    try:
-        H_inv = np.linalg.inv(np.eye(n)/l2(x) - np.outer(a,a)/(l2(x)**3))
-    except np.linalg.LinAlgError:
-        # If matrix is singular add low amplitude random noise to diagonal and try again
-        w = np.diag(np.random.normal(0, 1e-6, n))
-        H_inv = np.linalg.inv(np.eye(n)/l2(x) - np.outer(a,a)/(l2(x)**3) + w)
-    return H_inv
+# Gradient of squared p-norm
+def spnorm_grad(x, p):
+    if np.linalg.norm(x, ord=p) < 1e-16:
+        return np.zeros(x.shape)
+    else:
+        return x * (abs(x)/np.linalg.norm(x, ord=p))**(p-2)
 
-def unit_regularizer(a):
-    return np.eye(len(a))
+# Gradient of squared conjugate p-norm
+def spnorm_conj_grad(x, p):
+    q = p/(p-1)
+    return spnorm_grad(x,q)
 
-
-
-# Simulation parameters
+# Simulation parameters, one period is 6.3259
 dt = 0.005
-t_end = 30 #6.3259
+t_end = 100
 
 # Integration algorithm: 0 = scipy, 1 = forward Euler
 algo = 0
@@ -40,10 +34,10 @@ algo = 0
 gamma = 3.5
 kp = 5
 kq = 5
-r_f = 0.25
-regularizer = unit_regularizer #l2_regularizer
-t_open = 30
-parameter_threshold = 0.1
+t_d = 0
+r_d = 0
+norm_order = 1.05
+t_open = t_end
 
 # Physical constants
 g = 1
@@ -85,9 +79,9 @@ def forward_euler(fun, t_span, y0):
     
     return OdeResult(t=t, y=y, success=success, message=message)
 
-def parameter_dynamics(xh, x, ah, t):
+def parameter_gradient_dynamics(xh, x, t):
     """
-        Dynamics of parameter estimate a hat
+        Dynamics of parameter estimate gradient
         ph : p hat, estimate of p
         qh: q hat, estimagte of q
         pe: estimate error for p
@@ -138,10 +132,9 @@ def parameter_dynamics(xh, x, ah, t):
                               [0, gf13_1[0], gf23_1[0], 0, gf13_2[0], gf23_2[0], 0, gf13_3[0], gf23_3[0]],
                               [0, gf13_1[1], gf23_1[1], 0, gf13_2[1], gf23_2[1], 0, gf13_3[1], gf23_3[1]]])
 
-    # Change in parameter estimate
-    r_l = gamma/(1+r_f*t)
-    dah = -r_l*regularizer(ah) @ del_Y.T @ (xh-x)
-    return dah
+    # Time dependent learning rate
+    r_l = gamma/(1+r*t)
+    return -r_l*del_Y.T @ (xh-x)
 
 def estimate_dynamics(xh, x, ah):
     """
@@ -199,9 +192,9 @@ def estimate_dynamics(xh, x, ah):
 
 
     # Change in state estimate
-    dxh = del_Y @ ah + np.hstack((kq*(x[:6]-xh[:6]), kp*(x[6:]-xh[6:])))
-    return dxh
+    return del_Y @ ah + np.hstack((kq*(x[:6]-xh[:6]), kp*(x[6:]-xh[6:])))
 
+# Gravitational force between particles
 def gforce_1(x1, x2, m):
     return g*m*(x1-x2)/(l2(x1-x2)**3)
 
@@ -210,6 +203,10 @@ def gforce_2(x1, x2, m):
 
 def gforce_3(x1, x2, m):
     return 3*g*m*(x1-x2)/(l2(x1-x2)**5)
+
+def hamiltonian(x):
+    return (-g*m1*m2/l2(x[0:2]-x[2:4], axis=0) - g*m2*m3/l2(x[2:4]-x[4:6], axis=0) - g*m1*m2/l2(x[0:2]-x[4:6], axis=0)
+            + l2(x[6:8], axis=0)/(2*m1) + l2(x[8:10], axis=0)/(2*m1) + l2(x[10:12], axis=0)/(2*m1))
 
 # Solve IVP
 if algo == 0:
@@ -225,29 +222,40 @@ else:
     status = "failed"
 print(f'IVP solver {status}: {sol.message}')
 
-# Initialize estimator
+# Initialize estimate of state, parameters and parameter gradient
 xh = np.zeros(sol.y.shape)
 ah = np.zeros((21, len(sol.t)))
+g_ah = np.zeros((21, len(sol.t)))
 
 xh[:,0] = y0
 a = np.array((0, 0 ,0, 0, 0, 0, 1/(2*m1), 1/(2*m2), 1/(2*m3),
-                    0, 0, 0, g*m1*m2, g*m2*m3, g*m1*m3, 0, 0, 0, 0, 0, 0))
-ah[:,0] = np.random.uniform(0.1, 1.9, 21)
-
-noise_gain = 0.1
+                    0, 0, 0, g*m1*m2, g*m2*m3, g*m1*m3, 0, 0, 0, 0, 0, 0))/2
+# noise_gain = 0.1
 # ah[:,0] = a + np.random.uniform(-noise_gain, noise_gain, 21)
+# ah[:,0] = np.random.uniform(0.1, 1.9, 21)
+ah[:,0] = np.zeros(21)
+g_ah[:,0] = spnorm_grad(ah[:,0], norm_order)
 
+r = 0
 # Do parameter estimation and simulate estimated dynamics
 for i in range(len(sol.t)-1):
-    del_t = sol.t[i+1] - sol.t[i]
-    xh[:,i+1] = xh[:,i] + estimate_dynamics(xh[:,i], sol.y[:,i], ah[:,i])*del_t
-    ah[:,i+1] = ah[:,i] + parameter_dynamics(xh[:,i], sol.y[:,i], ah[:,i], sol.t[i])*del_t
-    # Stop learning after  t_open seconds
+    delta_t = sol.t[i+1] - sol.t[i]
+    xh[:,i+1] = xh[:,i] + estimate_dynamics(xh[:,i], sol.y[:,i], ah[:,i])*delta_t
+    g_ah[:,i+1] = g_ah[:,i] + parameter_gradient_dynamics(xh[:,i], sol.y[:,i], sol.t[i])*delta_t
+    ah[:,i+1] = spnorm_conj_grad(g_ah[:,i+1], norm_order)
+    
+    # Open control loop and end adaption after t_open seconds
     if sol.t[i] > t_open:
-        kp, kq = 0, 0
+        kp = 0
+        kq = 0
+        gamma = 0
+
+    # Decrease learning rate after t_d
+    if sol.t[i] > t_d:
+        r = r_d
 
 # Plot position trajectory
-plt.figure(1)
+plt.figure()
 for i in range(0,6):
     plt.plot(sol.t, sol.y[i], color=viridis(i/6))
     plt.plot(sol.t, xh[i], linestyle="--", color=viridis(i/6))
@@ -256,7 +264,7 @@ plt.xlabel("$t$")
 plt.ylabel("$q$")
 
 # Plot velocity trajectory
-plt.figure(2)
+plt.figure()
 for i in range(6,12):
     plt.plot(sol.t, sol.y[i], color=viridis((i-6)/6))
     plt.plot(sol.t, xh[i], linestyle="--", color=viridis((i-6)/6))
@@ -265,37 +273,56 @@ plt.xlabel(r"$t$")
 plt.ylabel(r"$p$")
 
 # Plot parameter estimate trajectory
-plt.figure(3)
+plt.figure()
 for i in range(ah.shape[0]):
     plt.plot(sol.t, ah[i,:])
 plt.title("Parameter estimate trajectory")
 plt.xlabel("time, $t$")
 plt.ylabel("$\hat{a}$")
 
-plt.figure(4)
+# Plot histogram of final parameter values
+plt.figure()
+plt.hist(ah[:,-1], bins=20)
+plt.title("Parameter distribution")
+plt.xlabel("Parameter value")
+plt.ylabel("Frequency")
+
+# Plot tracking error
+plt.figure()
 xe = np.sum(np.abs(sol.y - xh), axis=0)
 plt.plot(sol.t, xe)
 plt.title("Tracking error")
 plt.xlabel("time, $t$")
 plt.ylabel(r"$\Vert \hat{x} - x \Vert_1$")
 
-plt.figure(5)
+# Plot parameter estimate error
+plt.figure()
 ae = np.sum(np.abs(ah.T - a).T, axis=0)
 plt.plot(sol.t, ae)
 plt.title("Parameter estimation error")
 plt.xlabel("time, $t$")
 plt.ylabel(r"$\Vert \hat{a} - a \Vert_1$")
 
-plt.figure(6)
-plt.plot(sol.t, np.linalg.norm(ah, axis=0))
+# Plot parameter penalization function
+plt.figure()
+plt.plot(sol.t, 0.5*np.linalg.norm(ah, ord=norm_order, axis=0)**2)
 plt.title("Penalization function")
 plt.xlabel("time, $t$")
-plt.ylabel(r"$\Vert \hat{a} \Vert$")
+plt.ylabel(r"$\frac{1}{2} \Vert \hat{a} \Vert_p^2$")
 
-plt.figure(7)
-plt.plot(sol.t, gamma/(1+r_f*sol.t))
+# Plot learning rate
+plt.figure()
+plt.plot(sol.t, gamma/(1+r_d*sol.t))
 plt.title("Learning rate")
 plt.xlabel("$t$")
 plt.ylabel(r"$\gamma$")
+
+# Plot Hamiltonian error
+plt.figure()
+he = hamiltonian(xh) - hamiltonian(sol.y)
+plt.plot(sol.t, he)
+plt.title("Hamiltonian error")
+plt.xlabel("$t$")
+plt.ylabel(r"$\hat{\mathcal{H}} - \mathcal{H}$")
 
 plt.show()
