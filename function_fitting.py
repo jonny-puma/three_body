5dcefec2fec20b081042a363c9eee249e9c5a56c
Johannes Giercksky Nilssen <johanngn@stud.ntnu.no>
2021-05-18 17:14:48 +0200

    Added test error

--- function_fitting.py
+++ function_fitting.py
@@ -2,14 +2,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-n_samples = 50
-dim_regressor = 500
-epochs = 300000
-rla = 1e-5
-rlb = 1e-5
-lp_regulizer = 1
-lambda_1 = 0.1
-lambda_2 = 0.1
+n_samples = 20
+dim_regressor = 600
+epochs = 1000000
+rla = 1e-4
+rlb = 1e-4
+lp_regulizer = 2
+lambda_1 = 0
+lambda_2 = 0
 
 def f(x):
     return x + np.sin(x)
@@ -36,9 +36,13 @@ np.random.seed(99)
 x = np.random.uniform(0, 10, n_samples)
 y = f(x)
 
-a = np.random.uniform(-1, 1, dim_regressor)
+xt = np.random.uniform(0, 10, n_samples)
+yt = f(xt)
+
+a = np.random.uniform(-0.01, 0.01, dim_regressor)
 b = np.random.uniform(-10, 0, dim_regressor)
 e = np.zeros(epochs)
+et = np.zeros(epochs)
 
 if lp_regulizer == 2:
     regulizer = l2_reg
@@ -55,17 +59,27 @@ for j in range(epochs):
         xi = x[i]
         yi = y[i]
         ReLU_i = ReLU(xi + b)
+        ReLU_i_a = ReLU_i@a
         G_b_i = grad_Y_b(xi + b)
-        d_a += -2*(yi - ReLU_i@a)*ReLU_i
-        d_b += -2*(yi - ReLU_i@a)*G_b_i*a
-        e[j] += (yi-ReLU_i@a)**2/n_samples
+        d_a += -2*(yi - ReLU_i_a)*ReLU_i
+        d_b += -2*(yi - ReLU_i_a)*G_b_i*a
+        e[j] += (yi-ReLU_i_a)**2
+        et[j] += (yt[i] - ReLU(xt[i] + b)@a)**2
 
-    e[j] += lambda_1*regulizer(a) + lambda_2*regulizer(b)
+    e[j] /= n_samples
+    et[j] /= n_samples
+    # e[j] += lambda_1*regulizer(a) + lambda_2*regulizer(b)
+    # et[j] += lambda_1*regulizer(a) + lambda_2*regulizer(b)
+
     a -= rla*(d_a/n_samples + lambda_1*grad_regulizer(a))
     b -= rlb*(d_b/n_samples + lambda_2*grad_regulizer(b))
 
 plt.figure()
-plt.plot(e)
+plt.semilogy(e)
+plt.semilogy(et)
+plt.legend(("Training error", "Test error"))
+plt.title("Squared fitting error")
+
 
 plt.figure()
 plt.hist(a, bins=30)
@@ -79,7 +93,9 @@ plt.figure()
 xp = np.linspace(0, 10, 100)
 yp = np.array([ReLU(xi + b)@a for xi in xp])
 plt.plot(xp, yp)
-plt.plot(xp, f(xp))
+plt.plot(xp, f(xp), linestyle="--")
 plt.plot(x, y, "xr")
+plt.legend(("Regressor approximation", "True function", "Training samples"))
+plt.title("Function approximation")
 plt.show()
 
